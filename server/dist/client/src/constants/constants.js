3e59b7894323760cf88ce69c803bfd5d6e528eec
fsoubes <franck.soubes@hotmail.fr>
2021-03-10 04:33:56 +0100

    Delete server/dist directory

--- server/dist/client/src/constants/constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.NAME = void 0;
-exports.NAME = "feelAgain";
-//# sourceMappingURL=constants.js.map
--- server/dist/helpers/dateToString.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.dateToString = void 0;
-const dateToString = (date) => new Date(date).toISOString();
-exports.dateToString = dateToString;
-//# sourceMappingURL=dateToString.js.map
